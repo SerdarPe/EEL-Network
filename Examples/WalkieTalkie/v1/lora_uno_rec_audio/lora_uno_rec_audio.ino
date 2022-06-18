0bf0493f6c545cc6e843a645911fd3f8ddae97ed
SerdarPe <61140890+SerdarPe@users.noreply.github.com>
2022-06-18 23:53:25 +0300

    Remove comments

--- Examples/WalkieTalkie/v1/lora_uno_rec_audio/lora_uno_rec_audio.ino
+++ Examples/WalkieTalkie/v1/lora_uno_rec_audio/lora_uno_rec_audio.ino
@@ -48,41 +48,23 @@ void startPlayback(unsigned char const *data)
 
   cli();
 
-  // Set up Timer 2 to do pulse width modulation on the speakerpin.
-  // Use internal clock (datasheet p.160)
   ASSR &= ~(_BV(EXCLK) | _BV(AS2));
 
-  // Set fast PWM mode  (p.157)
   TCCR2A |= _BV(WGM21) | _BV(WGM20);
   TCCR2B &= ~_BV(WGM22);
-
-  // Do non-inverting PWM on pin OC2A (p.155)
-  // On the Arduino this is pin 11.(10 for MEGA)
   TCCR2A = (TCCR2A | _BV(COM2A1)) & ~_BV(COM2A0);
   TCCR2A &= ~(_BV(COM2B1) | _BV(COM2B0));
-
-  // No prescaler (p.158)
   TCCR2B = (TCCR2B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
 
-  // Set initial pulse width to the first sample.
-  //OCR2A = sounddata_data[0]; //data[0];
   OCR2A = 0;
 
-  // Set up Timer 1 to send a sample every interrupt.
-  // Set CTC mode (Clear Timer on Compare Match) (p.133)
-  // Have to set OCR1A *after*, otherwise it gets reset to 0!
   TCCR1B = (TCCR1B & ~_BV(WGM13)) | _BV(WGM12);
   TCCR1A = TCCR1A & ~(_BV(WGM11) | _BV(WGM10));
 
-  // No prescaler (p.134)
   TCCR1B = (TCCR1B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
 
-  // Set the compare register (OCR1A).
-  // OCR1A is a 16-bit register, so we have to do this with
-  // interrupts disabled to be safe.
   OCR1A = F_CPU / SAMPLE_RATE;    // 16e6 / 8000 = 2000
 
-  // Enable interrupt when TCNT1 == OCR1A (p.136)
   TIMSK1 |= _BV(OCIE1A);
 
   sei();
--- Examples/WalkieTalkie/v1/lora_uno_send_audio/lora_uno_send_audio.ino
+++ Examples/WalkieTalkie/v1/lora_uno_send_audio/lora_uno_send_audio.ino
@@ -57,58 +57,29 @@ ISR(ADC_vect) {
 }
 
 void setUpADC() {
-  // clear ADLAR in ADMUX (0x7C) to right-adjust the result
-  // ADCL will contain lower 8 bits, ADCH upper 2 (in last two bits)
   ADMUX &= B11011111;
   ADMUX |= B00100000;
 
-  // Set REFS1..0 in ADMUX (0x7C) to change reference voltage to the
-  // proper source (01)
   ADMUX |= B01000000;
 
-  // Clear MUX3..0 in ADMUX (0x7C) in preparation for setting the analog
-  // input
   ADMUX &= B11110000;
 
-  // Set MUX3..0 in ADMUX (0x7C) to read from AD8 (Internal temp)
-  // Do not set above 15! You will overrun other parts of ADMUX. A full
-  // list of possible inputs is available in Table 24-4 of the ATMega328
-  // datasheet
   ADMUX &= B11110000;  //A0
-  // ADMUX |= B00001000; // Binary equivalent
 
-  // Set ADEN in ADCSRA (0x7A) to enable the ADC.
-  // Note, this instruction takes 12 ADC clocks to execute
   ADCSRA |= B10000000;
 
-  // Set ADATE in ADCSRA (0x7A) to enable auto-triggering.
   ADCSRA |= B00100000;
 
-  // Clear ADTS2..0 in ADCSRB (0x7B) to set trigger mode to free running.
-  // This means that as soon as an ADC has finished, the next will be
-  // immediately started.
   ADCSRB &= B11111000;
 
-  // Set the Prescaler to 128 (16000KHz/128 = 125KHz)
-  // Above 200KHz 10-bit results are not reliable.
   ADCSRA |= B00000111;
 
-  // Set ADIE in ADCSRA (0x7A) to enable the ADC interrupt.
-  // Without this, the internal interrupt will not trigger.
   ADCSRA |= B00001000;
 
-  // Set ADPS to 4 (16MHz / 32 = 500KHz)
-  // About 38.4k sampling rate
-  ADCSRA |= B00000111;
-  ADCSRA &= B11111111;
+  ADCSRA &= B11111111;	//Change PS
 
-  // Enable global interrupts
-  // AVR macro included in <avr/interrupts.h>, which the Arduino IDE
-  // supplies by default.
   sei();
 
-  // Kick off the first ADC
-  // Set ADSC in ADCSRA (0x7A) to start the ADC conversion
   ADCSRA |= B01000000;
 }
 unsigned long tStart;
